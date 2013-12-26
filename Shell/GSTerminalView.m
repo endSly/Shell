577bad018142757642ac1fcbe17272580682e929
Endika Gutiérrez Salas <me@endika.net>
2013-12-26 21:59:37 +0100

    Added terminal view

--- Shell/GSTerminalView.m
+++ Shell/GSTerminalView.m
@@ -16,9 +16,9 @@
 
 @implementation GSTerminalView
 
-- (id)init
+- (id)initWithCoder:(NSCoder *)aDecoder
 {
-    self = [super init];
+    self = [super initWithCoder:aDecoder];
 
     if (self) {
         UIWebView *webView = [[UIWebView alloc] initWithFrame:self.frame];
@@ -26,14 +26,22 @@
         webView.delegate = self;
         [self addSubview:webView];
         self.webView = webView;
-    }
 
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(keyboardWillShow:)
+                                                     name:UIKeyboardWillShowNotification
+                                                   object:nil];
+    }
+    
     return self;
 }
 
 - (void)layoutSubviews
 {
     self.webView.frame = self.frame;
+
+    NSURLRequest *terminalLoadRequest = [NSURLRequest requestWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"terminal" ofType:@"html"] isDirectory:NO]];
+    [self.webView loadRequest:terminalLoadRequest];
 }
 
 #pragma mark - Web view delegate
@@ -45,6 +53,46 @@
     }
 
     return YES;
+}
+
+#pragma mark - Remove keyboard
+
+- (void)keyboardWillShow:(NSNotification *)note {
+    [self performSelector:@selector(removeBar) withObject:nil afterDelay:0];
+}
+- (void)removeBar {
+    // Locate non-UIWindow.
+    UIWindow *keyboardWindow = nil;
+    for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
+        if (![[window class] isEqual:[UIWindow class]]) {
+            keyboardWindow = window;
+            break;
+        }
+    }
+    UIView *keyboardView = nil;
+
+    // Locate UIWebFormView.
+    for (UIView *formView in [keyboardWindow subviews]) {
+        // iOS 5 sticks the UIWebFormView inside a UIPeripheralHostView.
+        if ([[formView description] rangeOfString:@"UIPeripheralHostView"].location != NSNotFound) {
+            keyboardView = formView;
+            for (UIView *subView in [formView subviews]) {
+                if ([subView.description rangeOfString:@"UIWebFormAccessory"].location != NSNotFound) {
+                    // remove the input accessory view
+                    [subView removeFromSuperview];
+                }
+            }
+        }
+    }
+
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, keyboardWindow.frame.size.width, 44.0f)];
+    toolbar.items = @[[[UIBarButtonItem alloc] initWithTitle:@"Tab" style:UIBarButtonItemStylePlain target:self action:@selector(test)]];
+    [keyboardView addSubview:toolbar];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
